62057c8331d422015a642b8590565bd0006cedb7
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-05-21 11:41:17 -0400

    Adding in slight styling with semantic ui

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,12 @@ import SongList from './SongList';
 
 const App = () => {
   return (
-    <div className="App">
-      Hello World!!!
-      <SongList />
+    <div className="App ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/SongList.js
+++ src/components/SongList.js
@@ -18,7 +18,7 @@ class SongList extends Component {
     }
   render() {
     return (
-      <div>
+      <div className="ui divided list">
         {this.renderList()}
       </div>
     )
